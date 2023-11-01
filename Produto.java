aeae7ef1040bd6bd75dc1d5b829e8a01f3313ff0
Pedro Henrique <92341351+Pedro-HCM@users.noreply.github.com>
2023-11-01 15:46:39 -0300

    Produto.java
    Classe Produto Criada para declarar as variáveis e valores presentes em funções do código Main.

---
+++ Produto.java
@@ -0,0 +1,58 @@
+package atividade;
+import java.util.Date;
+
+ public class Produto {
+    private String nome;
+    private String codigoDeBarras;
+    private int quantidade;
+    private double precoUnitario;
+    private Date dataDeValidade;
+
+    public Produto(String nome, String codigoDeBarras, int quantidade, double precoUnitario, Date dataDeValidade) {
+        this.nome = nome;
+        this.codigoDeBarras = codigoDeBarras;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+        this.dataDeValidade = dataDeValidade;
+    }
+
+    public String getCodigoDeBarras() {
+        return codigoDeBarras;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Date getDataDeValidade() {
+        return dataDeValidade;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setQuantidade(int novaQuantidade) {
+        this.quantidade = novaQuantidade;
+    }
+
+    public void setDataDeValidade(Date novaDataValidade) {
+        this.dataDeValidade = novaDataValidade;
+    }
+
+    public void setNome(String novoNome) {
+        this.nome = novoNome;
+    }
+
+    public void setCodigoDeBarras(String novoCodigoDeBarras) {
+        this.codigoDeBarras = novoCodigoDeBarras;
+    }
+
+    public void setPrecoUnitario(double novoPrecoUnitario) {
+        this.precoUnitario = novoPrecoUnitario;
+    }
+}
